32e70b396f390d7c5f7ffa1c95b0fd00c5c2f9e1
micdenny <micdenny@gmail.com>
2017-03-29 16:34:10 +0200

    concurrency support for PublishRunner

--- EasyNetQPerformanceTest/Runners/PublishRunner.cs
+++ EasyNetQPerformanceTest/Runners/PublishRunner.cs
@@ -1,6 +1,8 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Diagnostics;
 using System.Threading;
+using System.Threading.Tasks;
 using EasyNetQ;
 using EasyNetQ.Topology;
 using EasyNetQPerformanceTest.Contract;
@@ -9,6 +11,10 @@ namespace EasyNetQPerformanceTest.Runners
 {
     public class PublishRunner : IRunner
     {
+        private long _publishCount = 0;
+        private long _minDuration = long.MaxValue;
+        private long _maxDuration = long.MinValue;
+
         private readonly ApplicationOptions _applicationOptions;
 
         public PublishRunner(ApplicationOptions applicationOptions)
@@ -31,63 +37,92 @@ namespace EasyNetQPerformanceTest.Runners
                     bus.Subscribe<PerfMessage>(subscriptionId, message => { }).Dispose();
                 }
 
-                long publishCount = 0;
-                long minDuration = long.MaxValue;
-                long maxDuration = long.MinValue;
-
                 // -------------------
                 //     PERF TEST
                 // -------------------
                 var sw = Stopwatch.StartNew();
                 if (_applicationOptions.HitsCount > 0)
                 {
-                    var swh = new Stopwatch();
-                    for (int i = 0; i < _applicationOptions.HitsCount; i++)
+                    if (_applicationOptions.Concurrency == -1)
                     {
-                        swh.Restart();
-
-                        bus.Publish(new PerfMessage());
-
-                        swh.Stop();
-
-                        publishCount++;
-
-                        if (swh.ElapsedTicks < minDuration)
+                        var tasks = new List<Task>(_applicationOptions.HitsCount);
+                        for (int i = 0; i < _applicationOptions.HitsCount; i++)
                         {
-                            minDuration = swh.ElapsedTicks;
+                            tasks.Add(Task.Run(() =>
+                            {
+                                ProfilePublish(bus);
+                            }));
                         }
-                        if (swh.ElapsedTicks > maxDuration)
+                        Task.WaitAll(tasks.ToArray());
+                    }
+                    else if (_applicationOptions.Concurrency == 0)
+                    {
+                        var swh = new Stopwatch();
+                        for (int i = 0; i < _applicationOptions.HitsCount; i++)
                         {
-                            maxDuration = swh.ElapsedTicks;
+                            ProfilePublish(bus);
                         }
+                    }
+                    else if (_applicationOptions.Concurrency > 0)
+                    {
+                        int counter = 0;
+                        var tasks = new List<Task>(_applicationOptions.Concurrency);
+                        for (int i = 0; i < _applicationOptions.Concurrency; i++)
+                        {
+                            tasks.Add(Task.Run(() =>
+                            {
+                                while (Interlocked.Increment(ref counter) <= _applicationOptions.HitsCount)
+                                {
+                                    ProfilePublish(bus);
+                                }
+                            }));
+                        }
+                        Task.WaitAll(tasks.ToArray());
                     }
                 }
                 else if (_applicationOptions.DurationInSeconds > 0)
                 {
-                    var cts = new CancellationTokenSource(TimeSpan.FromSeconds(_applicationOptions.DurationInSeconds));
-                    var swh = new Stopwatch();
-                    while (!cts.Token.IsCancellationRequested)
+                    if (_applicationOptions.Concurrency == -1)
                     {
-                        swh.Restart();
-
-                        bus.Publish(new PerfMessage());
-
-                        swh.Stop();
-
-                        publishCount++;
-
-                        if (swh.ElapsedTicks < minDuration)
+                        var tasks = new List<Task>();
+                        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(_applicationOptions.DurationInSeconds));
+                        while (!cts.Token.IsCancellationRequested)
                         {
-                            minDuration = swh.ElapsedTicks;
+                            tasks.Add(Task.Run(() =>
+                            {
+                                ProfilePublish(bus);
+                            }));
                         }
-                        if (swh.ElapsedTicks > maxDuration)
+                        Task.WaitAll(tasks.ToArray());
+                    }
+                    else if (_applicationOptions.Concurrency == 0)
+                    {
+                        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(_applicationOptions.DurationInSeconds));
+                        var swh = new Stopwatch();
+                        while (!cts.Token.IsCancellationRequested)
                         {
-                            maxDuration = swh.ElapsedTicks;
+                            ProfilePublish(bus);
                         }
+                    }
+                    else if (_applicationOptions.Concurrency > 0)
+                    {
+                        var cts = new CancellationTokenSource(TimeSpan.FromSeconds(_applicationOptions.DurationInSeconds));
+                        var tasks = new List<Task>(_applicationOptions.Concurrency);
+                        for (int i = 0; i < _applicationOptions.Concurrency; i++)
+                        {
+                            tasks.Add(Task.Factory.StartNew(() =>
+                            {
+                                while (!cts.Token.IsCancellationRequested)
+                                {
+                                    ProfilePublish(bus);
+                                }
+                            }, TaskCreationOptions.LongRunning));
+                        }
+                        Task.WaitAll(tasks.ToArray(), TimeSpan.FromSeconds(_applicationOptions.DurationInSeconds));
                     }
                 }
                 sw.Stop();
-                var hitsPerSecond = (int)(publishCount / sw.Elapsed.TotalSeconds);
+                var hitsPerSecond = (int)(_publishCount / sw.Elapsed.TotalSeconds);
 
                 // clean up
                 if (_applicationOptions.CleanAfterTestDone)
@@ -99,12 +134,34 @@ namespace EasyNetQPerformanceTest.Runners
                     bus.Advanced.ExchangeDelete(new Exchange(exchangeName));
                 }
 
-                return new Stats(publishCount, hitsPerSecond, sw.ElapsedMilliseconds, minDuration, maxDuration);
+                return new Stats(_publishCount, hitsPerSecond, sw.ElapsedMilliseconds, _minDuration, _maxDuration);
             }
             finally
             {
                 bus.Dispose();
             }
+        }
+
+        public Stopwatch ProfilePublish(IBus bus)
+        {
+            var swh = Stopwatch.StartNew();
+
+            bus.Publish(new PerfMessage());
+
+            swh.Stop();
+
+            Interlocked.Increment(ref _publishCount);
+
+            if (swh.ElapsedTicks < _minDuration)
+            {
+                Interlocked.Exchange(ref _minDuration, swh.ElapsedTicks);
+            }
+            if (swh.ElapsedTicks > _maxDuration)
+            {
+                Interlocked.Exchange(ref _maxDuration, swh.ElapsedTicks);
+            }
+
+            return swh;
         }
     }
 }
